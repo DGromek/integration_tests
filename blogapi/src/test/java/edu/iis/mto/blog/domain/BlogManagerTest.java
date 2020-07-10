bdd667e250ec7518710da523cb491cab3ef4cc27
DGromek <dawidgromek.1997@gmail.com>
2020-07-10 19:50:53 +0200

    Added BlogManagerTest; Added new DomainError case.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,24 +1,29 @@
 package edu.iis.mto.blog.domain;
 
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.verify;
-
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.mapper.BlogDataMapper;
+import edu.iis.mto.blog.services.BlogService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.domain.model.AccountStatus;
-import edu.iis.mto.blog.domain.model.User;
-import edu.iis.mto.blog.domain.repository.UserRepository;
-import edu.iis.mto.blog.mapper.BlogDataMapper;
-import edu.iis.mto.blog.services.BlogService;
+import java.util.Optional;
+
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +31,9 @@ public class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+
+    @MockBean
+    private BlogPostRepository blogPostRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -44,4 +52,25 @@ public class BlogManagerTest {
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test(expected = DomainError.class)
+    public void addingLikeByUserWithNewAccountStatusShouldThrowError() {
+        User likingUser = new User();
+        likingUser.setId(100L);
+        likingUser.setAccountStatus(AccountStatus.NEW);
+
+        User userCreator = new User();
+        userCreator.setId(200L);
+        userCreator.setAccountStatus(AccountStatus.CONFIRMED);
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setId(100L);
+        blogPost.setUser(userCreator);
+
+        Mockito.when(userRepository.findById(likingUser.getId()))
+               .thenReturn(Optional.of(likingUser));
+        Mockito.when(blogPostRepository.findById(blogPost.getId()))
+               .thenReturn(Optional.of(blogPost));
+
+        blogService.addLikeToPost(likingUser.getId(), blogPost.getId());
+    }
 }
